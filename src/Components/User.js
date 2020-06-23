0162d84fe7336f860081deed367067e26ced5008
Ahsan Shakeel <ahsanshakeel1994@gmail.com>
2020-06-23 23:22:53 +0500

    Making Components

---
+++ src/Components/User.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const Username = () => {
+  const [username, setUsername] = userState("");
+  return (
+    <div>
+      <h1>Username</h1>
+    </div>
+  );
+};
+
+export default Username;
