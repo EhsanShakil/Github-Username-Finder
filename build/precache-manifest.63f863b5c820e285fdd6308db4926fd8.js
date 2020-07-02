6d4028502ba290ea518c0ee373326a31e4d4ca97
Ahsan Shakeel <ahsanshakeel1994@gmail.com>
2020-07-02 23:14:56 +0500

    Creating Build

---
+++ build/precache-manifest.63f863b5c820e285fdd6308db4926fd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cc251bd56f1996028d0a932f28119cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "feb8deacaf317da22b6d",
+    "url": "/static/css/main.d914ba5a.chunk.css"
+  },
+  {
+    "revision": "d1cba8e636193bb2aca9",
+    "url": "/static/js/2.6d56230f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6d56230f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feb8deacaf317da22b6d",
+    "url": "/static/js/main.46b4fbf9.chunk.js"
+  },
+  {
+    "revision": "f09b5907c517dc7a0aee",
+    "url": "/static/js/runtime-main.cbabf474.js"
+  }
+]);
